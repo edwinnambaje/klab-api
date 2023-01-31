444bdde2a38267f85316b62ba255e14cdbac72a3
edwinnambaje <nambajeeedwin@gmail.com>
2023-01-31 09:03:03 +0200

    added user controllers

---
+++ controllers/userController.js
@@ -0,0 +1,36 @@
+const User=require('../models/User');
+
+exports.getAll=async(req,res)=>{
+    try {
+        const user=await User.find()
+        res.status(200).json({message:"All users",data:user})
+    } catch (error) {
+        res.status(500).json({message:"Not found"})
+    }
+}
+exports.getById=async(req,res)=>{
+    try {
+        const user=await User.findById(req.params.id)
+        res.status(200).json({message:"User by Id",data:user})
+    } catch (error) {
+        res.status(500).json({message:"Not found"})
+    }
+}
+exports.deleteById=async(req,res)=>{
+    try {
+        const user=await User.findByIdAndDelete(req.params.id)
+        res.status(200).json({message:"User deleted Successfully"})
+    } catch (error) {
+        res.status(500).json({message:"Not found"})
+    }
+}
+exports.updateById=async(req,res)=>{
+    try {
+        const updatedUser=await User.findByIdAndUpdate(req.params.id,{
+            $set:req.body
+        },{new:true})
+        res.status(200).json({message:"User Updated Successfully",data:updatedUser})
+    } catch (error) {
+        res.status(500).json({message:"Not found"})
+    }
+}
--- routes/user.js
+++ routes/user.js
@@ -1,39 +1,10 @@
 const router=require('express').Router();
 const User=require('../models/User');
-const verifyToken=require('../middleware/auth')
+const verifyToken=require('../middleware/auth');
+const userController=require('../controllers/userController');
 
-router.get('/all',verifyToken.verifyToken,async(req,res)=>{
-    try {
-        const user=await User.find()
-        res.status(200).json({message:"All users",data:user})
-    } catch (error) {
-        res.status(500).json({message:"Not found"})
-    }
-})
-router.get('/:id',verifyToken.verifyToken,async(req,res)=>{
-    try {
-        const user=await User.findById(req.params.id)
-        res.status(200).json({message:"User by Id",data:user})
-    } catch (error) {
-        res.status(500).json({message:"Not found"})
-    }
-})
-router.delete('/:id',verifyToken.verifyToken,async(req,res)=>{
-    try {
-        const user=await User.findByIdAndDelete(req.params.id)
-        res.status(200).json({message:"User deleted Successfully"})
-    } catch (error) {
-        res.status(500).json({message:"Not found"})
-    }
-})
-router.put('/:id',verifyToken.verifyToken,async(req,res)=>{
-    try {
-        const updatedUser=await User.findByIdAndUpdate(req.params.id,{
-            $set:req.body
-        },{new:true})
-        res.status(200).json({message:"User Updated Successfully",data:updatedUser})
-    } catch (error) {
-        res.status(500).json({message:"Not found"})
-    }
-})
+router.get('/all',verifyToken.verifyToken,userController.getAll);
+router.get('/:id',verifyToken.verifyToken,userController.getById);
+router.delete('/:id',verifyToken.verifyToken,userController.deleteById);
+router.put('/:id',verifyToken.verifyToken,userController.updateById);
 module.exports=router;
