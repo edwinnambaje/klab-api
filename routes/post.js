79863939ad05a2960a149f52f85e8a2090d96bb8
edwinnambaje <nambajeeedwin@gmail.com>
2023-01-31 09:13:43 +0200

    added post controllers

--- routes/post.js
+++ routes/post.js
@@ -1,53 +1,10 @@
 const router=require('express').Router();
-
 const Post=require('../models/Post');
+const postController=require('../controllers/postController');
 
-router.post('/create',async(req,res)=>{
-    try {
-        const post= new Post({
-            title:req.body.title,
-            description:req.body.description,
-            image:req.body.image
-        })
-        await post.save();
-        res.status(200).json({message:"Post created",data:post})
-    } catch (error) {
-        res.status(401).json({message:error})
-    }
-});
-router.get('/:id',async(req,res)=>{
-    try {
-        const post=await Post.findById(req.params.id);
-        res.status(200).json({data:post})
-    } catch (error) {
-        res.status(401).json({message:error})
-    }
-});
-router.put('/update/:id',async(req,res)=>{
-    try {
-        const updatedPost=await Post.findByIdAndUpdate(req.params.id,{
-            $set:req.body
-        },{new:true})
-        await updatedPost.save();
-        res.status(200).json({message:"Post updated successfuly",data:updatedPost})
-    } catch (error) {
-        res.status(401).json({message:error})
-    }
-});
-router.delete('/delete/:id',async(req,res)=>{
-    try {
-        await Post.findByIdAndDelete(req.params.id);
-        res.status(200).json({message:"Post deleted successfuly"})
-    } catch (error) {
-        res.status(401).json({message:error})
-    }
-});
-router.get('/',async(req,res)=>{
-    try {
-        const post=await Post.find();
-        res.status(200).json({message:"Fetched Posts successfully",data:post})
-    } catch (error) {
-        res.status(401).json({message:error})
-    }
-})
+router.post('/create',postController.create);
+router.get('/:id',postController.getById);
+router.put('/update/:id',postController.updateById);
+router.delete('/delete/:id',postController.deleteById);
+router.get('/',postController.getAll);
 module.exports=router;
