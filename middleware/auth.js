b7c715fe112f4908345a928438b416b9ab39eaf5
edwinnambaje <nambajeeedwin@gmail.com>
2023-02-16 09:36:20 +0200

    added auth

--- middleware/auth.js
+++ middleware/auth.js
@@ -2,7 +2,7 @@ const verify =require('../helpers/jwt');
 
 const verifyToken=async(req,res,next)=>{
     try {
-        const authHeader = req.headers.token || req.headers['authorization'];
+        const authHeader = req.headers.token || req.headers.Authorization;
         if(!authHeader){
             return res.status(401).json({message:"You are not authenticated 1"});
         }
