a2adeb4fd9aa5a0650e17d8253e8121d4598053f
edwinnambaje <nambajeeedwin@gmail.com>
2023-06-08 01:25:18 +0200

    new changes to comment controller

--- controllers/commentController.js
+++ controllers/commentController.js
@@ -1,34 +1,29 @@
-
-const Post=require('../models/Post');
-const Comment=require('../models/Comment')
-const User=require('../models/User')
-exports.createComment=async (req, res) => {
-    //Find a POst
-    const user = await User.findById(req.user._id);
-    if (!user) {
-      return res.status(404).send({ error: "User not found" });
-    }
-    const post = await Post.findById(req.params.id);
-    if(!post)return res.status(400).json({message:"Post not Found"})
-  //Create a Comment
+const Post = require('../models/Post')
+const Comment = require('../models/Comment')
+const User = require('../models/User')
+exports.createComment = async (req, res) => {
+  const user = await User.findById(req.user._id)
+  if (!user) {
+    return res.status(404).send({ error: 'User not found' })
+  }
+  const post = await Post.findById(req.params.id)
+  if (!post) return res.status(400).json({ message: 'Post not Found' })
   const comment = new Comment({
-    // username:user.username,
-    comment:req.body.comment,
-    post:req.params.id,
-    user:user.username,
-    userId:user._id
-  });
-  await comment.save();
-  //Associate Post with comment
-  post.comments.push(comment);
-  await post.save();
-  return res.send(comment);
-};
-exports.gettComment=async (req,res)=>{
-  const comment=await Comment.find();
+    comment: req.body.comment,
+    post: req.params.id,
+    user: user.username,
+    userId: user._id
+  })
+  await comment.save()
+  post.comments.push(comment)
+  await post.save()
+  return res.send(comment)
+}
+exports.gettComment = async (req, res) => {
+  const comment = await Comment.find()
   res.send(comment)
 }
-exports.deleteComment= async (req,res)=>{
-  await Comment.findByIdAndDelete(req.params.commentId);
-  res.status(200).json({message:'Deleted successfully'})
+exports.deleteComment = async (req, res) => {
+  await Comment.findByIdAndDelete(req.params.commentId)
+  res.status(200).json({ message: 'Deleted successfully' })
 }
